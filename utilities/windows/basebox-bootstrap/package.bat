2c14b7633cc633b57b7db441f35df2f1ccf966a1
unknown <waynea@PU-WS-129.queen.pageup.com.au>
2013-07-16 19:12:02 +1000

    add: packaging code

---
+++ utilities/windows/basebox-bootstrap/package.bat
@@ -0,0 +1,21 @@
+@echo off
+set I=echo [INFO] 
+%I% Starting Windows Basebox Export and Install
+%I% 
+set BASE_BOX=%1
+set VAGRANT_BOX_NAME=%2
+REM default to %SYSTEMDRIVE%\vagrant allow option to specify dir
+set VAGRANT_DIR=%SYSTEMDRIVE%\vagrant
+set VAGRANT_BOX=%VAGRANT_DIR%\%VAGRANT_BOX_NAME%.box
+rem vagrant package --base "C:\Users\gibbonsd\VirtualBox VMs\win2008\win2008.vbox" --output c:\vagrant\win2008.box
+
+%I% Creating new Vagrant Box "%VAGRANT_BOX%" from the Virtual Box file "%BASE_BOX%"
+vagrant package --base "%BASE_BOX%" --output "%VAGRANT_BOX%"
+
+%I% 
+%I% Adding "%VAGRANT_BOX_NAME%" to Vagrant
+vagrant box add "%VAGRANT_BOX_NAME%" "%VAGRANT_BOX%"
+
+%I% 
+%I% Generating Vagrant File
+call set_vagrant_value MYNAME MYBOX
---
+++ utilities/windows/basebox-bootstrap/set_vagrantfile_value.bat
@@ -0,0 +1,25 @@
+@echo off
+setlocal enabledelayedexpansion
+set INTEXTFILE=vagrantfile.template
+set TEMPTEXTFILE=temp
+set OUTTEXTFILE=vagrantfile
+set SETBOX=BOXNAME
+set REPLACEBOX=%1
+set SETBOXURL=BOXURL
+set REPLACEBOXURL=%2
+set SEARCHTEXT=BOXNAME
+set REPLACETEXT=BATNAME
+del %OUTTEXTFILE%
+
+for /f "tokens=1,* delims=¶" %%A in ( '"type %INTEXTFILE%"') do (
+	set string=%%A
+	set modified=!string:%SETBOX%=%REPLACEBOX%!
+	echo. !modified! >> %TEMPTEXTFILE%
+)
+
+for /f "tokens=1,* delims=¶" %%A in ( '"type %TEMPTEXTFILE%"') do (
+	set string=%%A
+	set modified=!string:%SETBOXURL%=%REPLACEBOXURL%!
+	echo. !modified! >> %OUTTEXTFILE%
+)
+del %TEMPTEXTFILE%
